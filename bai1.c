d8a7dbe6ed36a098cf1758ea798941be53ec22b5
Khangle081020 <khangle08102004@gmail.com>
2026-02-05 08:40:43 +0700

    com108-cs21301-lab6-Khangle08102004

--- bai1.c
+++ bai1.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Lê Bá Tấn Khang]
+ * MSSV:      [PS48328]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
 //  BÀI 1: TÍNH TRUNG BÌNH TỔNG CÁC SỐ CHIA HẾT CHO 3 TRONG MẢNG 
@@ -10,4 +10,40 @@
 
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void randomArray(int arr[], int size, int min, int max) {
+    srand(time(NULL));
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % (max - min + 1) + min;
+    }
+}
+
+
+void printArray(int arr[], int size) {
+    printf("Các phần tử của mảng là:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+int main() {
+    int n;
+    
+    printf("Nhập số lượng phần tử của mảng: ");
+    scanf("%d", &n);
+    
+    int arr[10];
+
+    randomArray(arr, n, 1, 100);
+    printArray(arr, n);
+
+    
+    return 0;
+}
 
